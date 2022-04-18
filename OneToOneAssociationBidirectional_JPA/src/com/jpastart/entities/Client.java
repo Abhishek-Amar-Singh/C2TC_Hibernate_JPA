e9bec503016fd3312e0eacc9b49e1d2f6c3b6342
Abhishek Amar Singh <abhisheksingh.120120@gmail.com>
2022-04-18 22:29:58 +0530

    first commit

---
+++ OneToOneAssociationBidirectional_JPA/src/com/jpastart/entities/Client.java
@@ -0,0 +1,31 @@
+//OneToOneAssociationBiDirectional
+
+package com.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client
+{
+	public static void main(String[] args)
+	{
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager entityManager = emfactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		Student student1 = new Student();
+		student1.setName("Ramesh");
+		Address homeAddress1 = new Address();
+		homeAddress1.setStreet("Gandhi Nagar");
+		homeAddress1.setCity("Mumbai");
+		homeAddress1.setState("Maharashtra");
+		homeAddress1.setZipCode(400012);
+		student1.setAddress(homeAddress1);//inject address into student
+		entityManager.persist(student1);//persist only student, no need to persist address explicitly
+		
+		entityManager.getTransaction().commit();
+		System.out.println("Added one student with address to the sheet.student and sheet.address");
+	}
+}
